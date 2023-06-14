f3ca0075a4f2310924de3527a71cdbbfa90c9134
bla118 <bla118@sfu.ca>
2023-06-14 16:21:35 -0700

    drone model

---
+++ src/components/DroneModel.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import * as Threlte from '@threlte/core'
+	import * as Three from 'three'
+	import * as Extra from '@threlte/extras';
+	
+	const gridHelper = new Three.GridHelper(20, 10);
+	const axesHelper = new Three.AxesHelper(10);
+
+	let camera = {
+		position: { x: 10, y: 10, z: 10 },
+		zoom: 40
+	}
+	let pointLight = {
+		position: { x: 0, y: 5, z: -15},
+		color: 'blue'
+	}
+	let drone = {
+		position: {x: 0, y: 0, z: 0},
+		scale: 300
+	}
+
+</script>
+  
+  <div class="scene">
+	<Threlte.Canvas rendererParameters={{ antialias: true }} >
+		<!-- <Threlte.Object3DInstance object={gridHelper} /> -->
+		<!-- <Threlte.Object3DInstance object={axesHelper} /> -->
+		<Threlte.OrthographicCamera {...camera}>
+			<Threlte.OrbitControls/>
+		</Threlte.OrthographicCamera>
+		
+		<Threlte.AmbientLight color="white" intensity={0.2}/>
+		<Threlte.PointLight intensity={1.5} {...pointLight}/>
+		
+		<Extra.GLTF url="/drone.glb" {...drone}/>
+		
+	</Threlte.Canvas>
+  </div>
+  
+  <style>
+	.scene {
+	  max-width: 100%;
+	  height: 75%;
+	  
+	  border: 1px solid white;
+	}
+  </style>
+  
--- src/routes/drone/+page.svelte
+++ src/routes/drone/+page.svelte
@@ -1,26 +1,28 @@
 <script>
-    import Drone from "../../components/drone.svelte";
+  import Drone from "../../components/drone.svelte";
+  import DroneModel from "../../components/DroneModel.svelte";
 </script>
-  
-  
+
+
 <body>
-    <div class="main">
-      canvas here
-      <Drone/>
-    </div>
-  </body>
-  
+<div class="main">
+    <DroneModel/>
+    <Drone/>
+  </div>
+</body>
+
 <style>
-    body{
-      background: black;
-      background-image: radial-gradient(white 1px, transparent 0);
-      background-size: 40px 40px;
-      background-position: -20px -20px;
-    }
-    .main{
-      color: white;
-      display:grid;
-      grid-template-columns: 1fr 1fr;
-      gap: 10px;
-    }  
+  body{
+    background: black;
+    background-image: radial-gradient(white 1px, transparent 0);
+    background-size: 40px 40px;
+    background-position: -20px -20px;
+  }
+  .main{
+    color: white;
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+  }  
+
 </style>
